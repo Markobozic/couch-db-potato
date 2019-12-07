40fe11b603cb2076cfb1bb6fbcde99e9b7d54772
Marko <bozic@pdx.edu>
2019-12-07 09:26:24 -0800

    adding docType to couchdb loop documents

--- src/importer.py
+++ src/importer.py
@@ -59,6 +59,7 @@ class CouchImporter:
                 datetime = row[1].replace(' ', '_')
 
                 document['_id'] = f'{row[0]}_{datetime}'
+                document['docType'] = 'loop'
                 document['starttime'] = row[1]
                 document['volume'] = row[2]
                 document['speed'] = row[3]
