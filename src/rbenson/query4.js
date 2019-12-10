e6dfff5c941a0a714d298c51ca18081473e63d6a
rkbjunior <rkbjunior@gmail.com>
2019-12-10 09:01:13 -0800

    Updated Query4.js

--- src/rbenson/query4.js
+++ src/rbenson/query4.js
@@ -1,21 +1,26 @@
 //Map Function
 function (doc) {
-    if (doc.docType == "loop" && doc.speed > 0 && doc.locationtext == "Sunnyside NB" && doc.starttime >= "2011-09-15 00:01:40-07" && doc.starttime <= "2011-09-15 00:08:40-07") {
-        emit(doc.starttime, { "station_length": doc.length, "total_speed": doc.speed });
-    }
+  if(doc.docType == "loop" && doc.speed > 0 && doc.locationtext == "Foster NB" && doc.starttime >= "2011-09-22 16:00:00-07" && doc.starttime <= "2011-09-22 18:00:00-07" ) {
+    emit(doc.starttime, {"station_length": doc.length, "total_speed": doc.speed, "count": 1});
+  }
 }
 
 //Reduce Function
-function (keys, values, rereduce) {
-    var result = { "TravelTime": 0, "travel_units": "seconds", "station_length": 0, "total_speed": 0, "avg_speed": 0 };
-
+function(keys, values, rereduce) {
+    var result = {
+        'TravelTime': 0,
+        'travel_units': 'seconds',
+        'station_length': 0,
+        'total_speed': 0,
+        'avg_speed': 0,
+        'count': 0,
+    };
     for (var i = 0; i < values.length; ++i) {
         result.total_speed += values[i].total_speed;
         result.station_length = values[i].station_length;
+        result.count += values[i].count;
     }
-
-    result.avg_speed = result.total_speed / values.length;
+    result.avg_speed = result.total_speed / result.count;
     result.TravelTime = ((result.station_length / result.avg_speed) * 3660);
-
     return result;
 }
